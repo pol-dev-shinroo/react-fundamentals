01609f0ab0a4e58aa12e73327c46e0609582ecaa
Paul Kim <manus.dream.language@gmail.com>
2022-06-20 23:22:44 +0900

    accessing Javascript in JSX

--- src/App.js
+++ src/App.js
@@ -5,38 +5,32 @@ const BookList = () => {
     return (
         <section className="booklist">
             <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
         </section>
     );
 };
 
+const myFunction = () => {
+    console.log(2);
+};
+
+const author = "Jenny Han";
 const Book = () => {
+    const title = "It's Not Summer Without You";
     return (
         <article className="book">
-            <Image />
-            <Title />
-            <Author />
-        </article>
-    );
-};
-const Image = () => {
-    return (
-        <div>
             <img
                 src="https://images-na.ssl-images-amazon.com/images/I/51ky4TIb1bL._SX331_BO1,204,203,200_.jpg"
                 alt=""
             />
-        </div>
+            <h1>{title}</h1>
+            <h4>{author.toUpperCase()}</h4>
+            <p>{console.log(1)}</p>
+            {/* However, below gives an error */}
+            {/* <p>{let x = 1; console.log(1)}</p> */}
+            <p>{myFunction()}</p>
+            <p>{6 + 6}</p>
+        </article>
     );
-};
-const Title = () => {
-    return <h1>It's Not Summer Without You</h1>;
-};
-const Author = () => {
-    return <h4>Jenny Han</h4>;
 };
 
 export default BookList;
