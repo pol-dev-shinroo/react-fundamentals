8ca906ef0940d5b8530ef0d6e57885a676795db2
Paul Kim <manus.dream.language@gmail.com>
2022-06-21 01:28:31 +0900

    addition to using spread operator

--- src/App.js
+++ src/App.js
@@ -26,9 +26,7 @@ const BookList = () => {
     );
 };
 
-const Book = (props) => {
-    console.log(props);
-    const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
     return (
         <article className="book">
             <img src={img} alt="" />
